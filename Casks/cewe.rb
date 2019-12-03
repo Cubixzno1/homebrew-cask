0bc5368377f31eb463003db7d4f56fda8b653861
Cubixzno1 <48513737+Cubixzno1@users.noreply.github.com>
2019-12-03 09:26:22 +0100

    Update cewe.rb

--- Casks/cewe.rb
+++ Casks/cewe.rb
@@ -1,6 +1,6 @@
 cask 'cewe' do
   version '6.4.4'
-  sha256 '35eed803d6d68e4684063717b285bb7e868080d85d62cb0e9bb05ed883a128f0'
+  sha256 '84dff218cc78e701091557fe2473acb3b9d4e760fa303da55df6a8a32f5f198f'
 
   # photoprintit.com/api/getClient/18299-da_DK/hps was verified as official when first introduced to the cask
   url 'https://dls.photoprintit.com/api/getClient/18299-da_DK/hps/om_seo_goo_x_18299_x_18299-AF9Ytche8LJh7/mac'
